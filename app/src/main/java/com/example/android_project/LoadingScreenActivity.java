559edc8587d5d014ee324866af684cf255b16504
OussamaOuakrim <83608212+OussamaOuakrim@users.noreply.github.com>
2022-04-05 13:12:58 +0000

    Create LoadingScreenActivity.java

---
+++ app/src/main/java/com/example/android_project/LoadingScreenActivity.java
@@ -0,0 +1,54 @@
+package com.addition.simpleapi;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+
+import android.app.ProgressDialog;
+import android.content.Context;
+
+import android.content.Intent;
+
+import android.os.Bundle;
+import android.os.Handler;
+
+
+public class LoadingScreenActivity extends AppCompatActivity {
+
+    private static final long SPLASH_SCREEN_TIMEOUT = 2000;
+    
+    ProgressDialog progressDialog;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_loading_screen);
+
+        getSupportActionBar().hide();
+        getWindow().setNavigationBarColor(ContextCompat.getColor(this, R.color.colorPrimaryDark));
+
+        //CheckForUpdate();
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                Intent intent = new Intent(LoadingScreenActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, SPLASH_SCREEN_TIMEOUT);
+    }
+    public void ShowDialog(Context context) {
+        //setting up progress dialog
+        progressDialog = new ProgressDialog(context);
+        progressDialog.show();
+        progressDialog.setContentView(R.layout.progress_dialog);
+        progressDialog.setCanceledOnTouchOutside(false);
+        progressDialog.getWindow().setBackgroundDrawableResource(android.R.color.transparent);
+    }
+
+    public void DismissDialog() {
+        progressDialog.dismiss();
+    }
+}
